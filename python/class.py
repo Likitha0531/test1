1066cc43bff8672c0687a229b34e6cc9f6215958
Ratnam <likithaadapa2000@gmail.com>
2022-11-17 22:42:53 +0530

    first

---
+++ python/class.py
@@ -0,0 +1,17 @@
+FirstName = input("enter your first name")
+LastName = input("enter last name")
+rollno = input("enter your hallticket number")
+
+lst = [int(x) for in input("enter number seperated by space").split()]
+lst
+output:[1,2,3] 
+s.replace(' ','')
+s.replace('text','').replace(' ','')
+s.count('a')// only small a is counted //
+s.upper() or) s.lower()
+s.title()
+
+
+up,low,title,split , strip , replace,len
+ 
+ 
